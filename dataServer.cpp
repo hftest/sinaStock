eae3a53f2d0972c73c155e829aabd8efe62c0258
Cheng <chenglv2007@gmail.com>
2013-04-19 21:44:11 +0000

    modified dataserver

--- dataServer.cpp
+++ dataServer.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,6 +17,8 @@ static int curlRead(char * url, char * result);
 int reply(int fd)
 {
 	char line[100];
+	pid_t child;
+	memset(line,'0',100*sizeof(char));
 	int readSize=0;
 	while(true)
 	{
@@ -25,10 +29,14 @@ int reply(int fd)
 		char month[3];
 		char day[3];
 		char stockno[7];
-		strncpy(line,year,4);
-		strncpy(line+5,month,2);
-		strncpy(line+8,day,2);
-		strncpy(line+11,year,6);
+		strncpy(year,line,4);
+		strncpy(month,line+5,2);
+		strncpy(day,line+8,2);
+		strncpy(stockno,line+11,6);
+		year[4]='\0';
+		month[2]='\0';
+		day[2]='\0';
+		stockno[6]='\0';
 		if(month[1]=='0')
 		{	
 			month[0]='0';
@@ -39,8 +47,7 @@ int reply(int fd)
 			month[1]--;
 		}
 		printf("##REQUEST##: %s-%s-%s for %s\n Processing...\n",year,month,day,stockno);
-		//char url[50]="http://ichart.yahoo.com/table.csv?s=600000.SS&a=08&b=25&c=2010&d=09&e=8&f=2010&g=d";
-		char url[50]="http://ichart.yahoo.com/table.csv?s=";
+		char url[200]="http://ichart.yahoo.com/table.csv?s=";
 		strcat(url,stockno);
 		strcat(url,".SS&a=");
 		strcat(url,month);
@@ -54,9 +61,17 @@ int reply(int fd)
 		strcat(url,day);
 		strcat(url,"&f=");
 		strcat(url,year);
-		curlRead(url,line);
-		printf("*: %s\n",line)
-		//write(fd,line,strlen(line));	
+		printf("#: %s\n",url);
+	//	if((child=fork())==0)
+	//	{
+			curlRead(url,line);
+			printf("curl finished\n");
+			printf("contain1: %s",line);
+			write(fd,line,3);	
+			printf("contain2: %s",line);
+	//		exit(0);
+		//}
+//		waitpid(child,NULL,0);
 	}
 }
 int main(int argc,char ** argv)
@@ -72,17 +87,19 @@ int main(int argc,char ** argv)
 	servaddr.sin_port=htons(9877);
 	bind(listenfd,(sockaddr *)&servaddr,sizeof(servaddr));
 	listen(listenfd,1024);
+	printf("Data server started\n");
 	while(true)
 	{
 
 		clilen=sizeof(cliaddr);
 		connfd=accept(listenfd,(sockaddr *)&cliaddr,&clilen);
-		if((childpid=fork())==0)
-		{
-			close(listenfd);
+	//	if((childpid=fork())==0)
+	//	{
+	//		close(listenfd);
 			reply(connfd);
-			exit(0);
-		}
+	//		exit(0);
+	//	}
+	//	waitpid(childpid,NULL,0);
 		close(connfd);
 	}
 	return 0;
@@ -108,28 +125,28 @@ bool curlInit(CURL * & conn,char *url,string *buffer)
 	printf("3");
 	if(code!=CURLE_OK)
 	{
-//		fprintf(stdout,"Failed to set url|%s|:|%s|\n",url,errorBuffer);
+		fprintf(stdout,"Failed to set url|%s|:|%s|\n",url,errorBuffer);
 		return false;
 	}
 	code=curl_easy_setopt(conn,CURLOPT_FOLLOWLOCATION,1);
 	printf("4");
 	if(code!=CURLE_OK)
 	{
-//		fprintf(stdout,"Fail to set redirection option |%s|\n",errorBuffer);
+		fprintf(stdout,"Fail to set redirection option |%s|\n",errorBuffer);
 		return false;
 	}
 	code=curl_easy_setopt(conn,CURLOPT_WRITEFUNCTION,writer);
 	printf("5");
 	if(code!=CURLE_OK)
 	{
-		//fprintf(stdout,"Failed to set writer |%s|\n",errorBuffer);
+		fprintf(stdout,"Failed to set writer |%s|\n",errorBuffer);
 		return false;
 	}
 	code=curl_easy_setopt(conn,CURLOPT_WRITEDATA,buffer);
 	printf("6");
 	if(code!=CURLE_OK)
 	{
-		//fprintf(stdout,"Failed to set buffer |%s|\n",errorBuffer);
+		fprintf(stdout,"Failed to set buffer |%s|\n",errorBuffer);
 		return false;
 	}
 	printf("7");
@@ -141,104 +158,7 @@ static int writer(char * data, size_t size,size_t nmemb,string *writerData)
 	unsigned long sizes=size * nmemb;
 	if(writerData==NULL) return 0;
 	int index=0;
-	char * data2=(char *)malloc(sizes*sizeof(char));
-	for(int i=0;i<sizes;i++)
-	{
-		int spaceF=0;
-		if((data[i]<32||data[i]>127)&&data[i]!='\n')
-		{
-			data2[index]=data[i];
-			index++;
-			spaceF=1;
-		}	
-		else {
-			if(spaceF==1)
-			{
-				data2[index]=' ';
-				index++;
-				spaceF=0;
-			}
-		}
-	}
-	writerData->append(data2,index);
-	free(data2);
-	return sizes;
-}
-bool curlInit(CURL * & conn,char *url,string *buffer) 
-{
-	CURLcode code;
-	conn=curl_easy_init();
-//	printf("1");
-	if(conn==NULL)
-	{
-		fprintf(stdout,"connection is NULL after creation\n");
-		return false;
-	}
-//	printf("2");
-	code=curl_easy_setopt(conn,CURLOPT_ERRORBUFFER,errorBuffer);
-	if(code!=CURLE_OK)
-	{
-		fprintf(stdout,"fail to set error buffer: %d\n",code);
-		return false;
-	}
-	code=curl_easy_setopt(conn,CURLOPT_URL,url);
-//	printf("3");
-	if(code!=CURLE_OK)
-	{
-		fprintf(stdout,"Failed to set url|%s|:|%s|\n",url,errorBuffer);
-		return false;
-	}
-	code=curl_easy_setopt(conn,CURLOPT_FOLLOWLOCATION,1);
-//	printf("4");
-	if(code!=CURLE_OK)
-	{
-		fprintf(stdout,"Fail to set redirection option |%s|\n",errorBuffer);
-		return false;
-	}
-	code=curl_easy_setopt(conn,CURLOPT_WRITEFUNCTION,writer);
-//	printf("5");
-	if(code!=CURLE_OK)
-	{
-		fprintf(stdout,"Failed to set writer |%s|\n",errorBuffer);
-		return false;
-	}
-	code=curl_easy_setopt(conn,CURLOPT_WRITEDATA,buffer);
-//	printf("6");
-	if(code!=CURLE_OK)
-	{
-		fprintf(stdout,"Failed to set buffer |%s|\n",errorBuffer);
-		return false;
-	}
-//	printf("7");
-	return true;
-}
-
-static int writer(char * data, size_t size,size_t nmemb,string *writerData)
-{
-	unsigned long sizes=size * nmemb;
-	if(writerData==NULL) return 0;
-	int index=0;
-	char * data2=new char[sizes];
-	int spaceF=0;
-	for(int i=0;i<sizes;i++)
-	{
-		if(((data[i]<32||data[i]>127)&&data[i]!='\n'&&data[i]!=13&&data[i]!=9)||(data[i]<='9'&&data[i]>='0')||data[i]=='.')
-		{
-			data2[index]=data[i];
-			index++;
-			spaceF=1;
-		}	
-		else {
-			if(spaceF==1)
-			{
-				data2[index]=' ';
-				index++;
-				spaceF=0;
-			}
-		}
-	}
-	writerData->append(data2,index);
-	delete data2;
+	writerData->append(data,index);
 	return sizes;
 }
 static int curlRead(char * url,char * result)
@@ -246,6 +166,7 @@ static int curlRead(char * url,char * result)
 	CURL *con=NULL;
 	CURLcode code;
 	string buffer;
+	printf("\n**1");
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	if(!curlInit(con,url,&buffer))
 	{
@@ -258,7 +179,8 @@ static int curlRead(char * url,char * result)
 		printf("Failed perform\n");
 		return -2;
 	}
-	strcpy(buffer.c_str(),result);
+	printf("9");
+	strcpy(result,buffer.c_str());
 	curl_easy_cleanup(con);
 	return 0;
 }
